bcbb962cc71988bb90688193a81e8b729a2483e0
andreijivan <andrei_jivian@yahoo.com>
2020-05-07 09:22:09 +0300

    test commit

--- src/main/java/ro/siit/MapAge.java
+++ src/main/java/ro/siit/MapAge.java
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 public class MapAge {
     public static long calculateAgeInDays(Date date){
-        Date todayDate = new Date(); //data zilei curente
+        Date todayDate = new Date(); //data zilei curente folosite la calcul total zile traite
         long totalDaysLived = todayDate.getTime() - date.getTime();
 
         return TimeUnit.DAYS.convert(totalDaysLived,TimeUnit.MILLISECONDS);
